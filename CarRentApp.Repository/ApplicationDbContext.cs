859cc295041c4df80dedc5112486578a81d68086
Enes Sejfovski <enes22seyfovski@gmail.com>
2024-09-08 12:50:47 +0200

    Little changes in the structure of the project.

--- CarRentApp.Repository/ApplicationDbContext.cs
+++ CarRentApp.Repository/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace CarRentApp.Repository
+namespace CarRentApp.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
